285d0a9ef1f1e4e38230bb2f6c9212e16e99d3d5
Gary Jackson <garyljackson@gmail.com>
2024-01-09 09:12:09 +1000

    Add example

--- 02-Prompt-Templates/Program.cs
+++ 02-Prompt-Templates/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+var azureOpenAiSettings = configuration.GetSection("AzureOpenAi");
+
+var endpoint = azureOpenAiSettings["Endpoint"];
+var apiKey = azureOpenAiSettings["ApiKey"];
+var chatCompletionDeploymentName = azureOpenAiSettings["ChatCompletionDeploymentName"];
+var chatCompletionModelId = azureOpenAiSettings["ChatCompletionModelId"];
+
+Console.Clear();
+
+var builder = Kernel.CreateBuilder();
+
+builder.Services
+    .AddLogging(loggingBuilder => loggingBuilder.AddDebug().SetMinimumLevel(LogLevel.Trace))
+    .AddAzureOpenAIChatCompletion(
+        chatCompletionDeploymentName!,
+        endpoint!,
+        apiKey!,
+        modelId: chatCompletionModelId);
+
+var kernel = builder.Build();
+
+var prompt = """
+             Rewrite the following in the style of Sherlock Holmes
+
+             {{$input}}
+             """;
+
+var inlineFunction = kernel.CreateFunctionFromPrompt(prompt);
+
+var kernelArguments = new KernelArguments { { "input", "The stars are so bright tonight" } };
+
+var result = await kernel.InvokeAsync(inlineFunction, kernelArguments);
+
+Console.WriteLine(result);
