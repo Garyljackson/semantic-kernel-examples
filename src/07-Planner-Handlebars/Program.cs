5b63e8fc9eea29b2e2e5799e07835061573fc553
Gary Jackson <garyljackson@gmail.com>
2024-01-11 11:04:30 +1000

    Add planner example

--- src/07-Planner-Handlebars/Program.cs
+++ src/07-Planner-Handlebars/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using System.Reflection;
+using _07_Planner.Plugins;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Planning.Handlebars;
 
 IConfiguration configuration = new ConfigurationBuilder()
     .AddJsonFile("AppSettings.json", optional: true, reloadOnChange: true)
@@ -26,4 +30,21 @@ builder.Services
         endpoint!,
         apiKey!);
 
+builder.Plugins.AddFromType<MathPlugin>();
+
 var kernel = builder.Build();
+
+ChatHistory history = [];
+
+var planner = new HandlebarsPlanner(new HandlebarsPlannerOptions() { AllowLoops = true });
+
+var plan = await planner.CreatePlanAsync(kernel, "Add 10 to 100 then subtract 15 and then divide by 2");
+
+Console.WriteLine("Plan steps");
+Console.WriteLine(plan);
+Console.WriteLine("----------");
+
+var result = (await plan.InvokeAsync(kernel, [])).Trim();
+
+Console.WriteLine("Result");
+Console.WriteLine(result);
